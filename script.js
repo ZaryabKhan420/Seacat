38595f9bd3d98f4b054bc868607bbe76fd832a69
ZaryabKhan420 <zaryabz043@gmail.com>
2023-10-05 16:47:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+Draggable.create("#dragme", {
+    type:"x", 
+    bounds:".controller", 
+    inertia:true
+});
+
+gsap.to("#dragme",{
+    rotate:"-360deg",
+    duration:2,
+})
+
+document.addEventListener("mousemove",()=>{
+    var dragger = document.querySelector("#dragme");
+    var target = document.querySelector("#target");
+    var video1 = document.querySelector(".video1");
+
+    var p1 = dragger.getBoundingClientRect();
+    var p2 = target.getBoundingClientRect();
+
+    if(p1.right < p2.left) {
+        video1.style.opacity = 1;
+    }
+    else{
+        video1.style.opacity = 0;
+
+    }
+})
+
+
+
+
